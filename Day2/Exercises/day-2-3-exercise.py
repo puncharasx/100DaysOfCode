18b90db9b92497dc79e9990d761ec3f04d910b1e
Puncharas <puncharasx@gmail.com>
2021-10-03 19:56:56 +0700

    :white_check_mark: Day2

---
+++ Day2/Exercises/day-2-3-exercise.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+age_int = int(age)
+years = 90 - age_int
+days =  years * 365
+weeks = years * 52
+months = years * 12
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+
+
+
+
+
+
+
+
